107fad8b0a5f331e95b8c7632a503ae1540b0689
patrickjunta <patrickjunta@gmail.com>
2021-03-11 10:29:46 +0100

    .env et mysqlConfi.js fait

--- backend/mysqlConfig.js
+++ backend/mysqlConfig.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql');
+require("dotenv").config();
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database : "test_db"
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database : process.env.DATABASE
   });
   db.connect(function(err) {
     if (err) throw err;
